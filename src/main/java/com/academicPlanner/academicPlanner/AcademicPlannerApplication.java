5e5359bd125c0dd92651e0ef0c68d757de737a46
Rebeca Souza <rebecasza.santos@gmail.com>
2019-09-21 23:52:08 -0300

    Iniciando o projeto atraves do Spring Initializr
    Esse commit cria a estrutura do projeto com gradle, spring web e devtools atraves da ferramenta Spring Initializr.

---
+++ src/main/java/com/academicPlanner/academicPlanner/AcademicPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.academicPlanner.academicPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicPlannerApplication.class, args);
+	}
+
+}
