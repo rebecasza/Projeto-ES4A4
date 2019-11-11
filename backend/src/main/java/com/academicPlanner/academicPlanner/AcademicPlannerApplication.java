d5d813bdaa8529fb680ad5bd6aecb737c2fa55e2
Nessaoana <nessaoana09@gmail.com>
2019-11-11 17:36:51 -0300

    add

--- backend/src/main/java/com/academicPlanner/academicPlanner/AcademicPlannerApplication.java
+++ backend/src/main/java/com/academicPlanner/academicPlanner/AcademicPlannerApplication.java
@@ -2,8 +2,16 @@ package com.academicPlanner.academicPlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "com.academicPlanner.academicPlanner.Model"
+        })
+@EnableJpaRepositories(basePackages = {
+        "com.academicPlanner.academicPlanner.Repository"
+        })
 public class AcademicPlannerApplication {
 
 	public static void main(String[] args) {
