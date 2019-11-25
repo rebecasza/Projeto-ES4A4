164636ae2a614058e303f9f57ecb299c9a77ba8f
Nessaoana <nessaoana09@gmail.com>
2019-11-25 17:56:55 -0300

    criando a classe Estudo com relacionamento

--- backend/src/main/java/com/academicPlanner/academicPlanner/Model/Estudo.java
+++ backend/src/main/java/com/academicPlanner/academicPlanner/Model/Estudo.java
@@ -1,12 +1,23 @@
-/*package com.academicPlanner.academicPlanner.Model;
+package com.academicPlanner.academicPlanner.Model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 public class Estudo {
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	@ManyToOne
 	private Materia materia;
 	private String dataInicio; 
 	private String dataFim;
@@ -45,4 +56,3 @@ public class Estudo {
 	
 	
 }
-*/
