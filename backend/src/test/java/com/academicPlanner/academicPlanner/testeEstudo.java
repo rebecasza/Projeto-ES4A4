9ff3273be82c039826d0bb3633bc4be5b0809d50
Erika-Garcia <erikagarcia2@gmail.com>
2019-12-11 20:17:37 -0300

    test

--- backend/src/test/java/com/academicPlanner/academicPlanner/testeEstudo.java
+++ backend/src/test/java/com/academicPlanner/academicPlanner/testeEstudo.java
@@ -3,10 +3,26 @@ package com.academicPlanner.academicPlanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.academicPlanner.academicPlanner.Model.Estudo;
 
 
-public class testeEstudo {
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AcademicPlannerApplication.class , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class testeEstudo {
+	  @LocalServerPort
+	    private int port;
+
+	    @Autowired
+	    private Estudo estudo;
+	    
 }
--- backend/src/test/java/com/academicPlanner/academicPlanner/testeUsuario.java
+++ backend/src/test/java/com/academicPlanner/academicPlanner/testeUsuario.java
@@ -1,12 +1,44 @@
 package com.academicPlanner.academicPlanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.academicPlanner.academicPlanner.Model.Usuario;
 
 
-public class testeUsuario {
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AcademicPlannerApplication.class , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class testeUsuario {
+	  @LocalServerPort
+	    private int port;
+
+	    @Autowired
+	    private Usuario usuario;
+	    
+	    @Autowired
+	    private TestRestTemplate restTemplate;
+	    
+	    private String getRootUrl() {
+	        return "http://localhost:" + port;
+	    }
+	    
+	    @Test
+	    public void testGetAllUsuario() {
+	        HttpHeaders headers = new HttpHeaders();
+	        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/usuarios",
+	                HttpMethod.GET, entity, String.class);
+	        assertNotNull(response.getBody());
+	    }
 }
