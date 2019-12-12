496f8aea5a81f5c4336db2de0bca2a052370f3dc
erika-garcia <erikagarcia2@gmail.com>
2019-12-11 22:25:55 -0300

    test

--- backend/src/test/java/com/academicPlanner/academicPlanner/testeEstudo.java
+++ backend/src/test/java/com/academicPlanner/academicPlanner/testeEstudo.java
@@ -1,18 +1,22 @@
 package com.academicPlanner.academicPlanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.expression.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.academicPlanner.academicPlanner.Model.Estudo;
+import com.academicPlanner.academicPlanner.Model.Materia;
+import com.academicPlanner.academicPlanner.Model.Usuario;
 
 
 
@@ -24,5 +28,38 @@ public class testeEstudo {
 
 	    @Autowired
 	    private Estudo estudo;
+	    
+	    private Usuario usuario;
+	    
+	    private Materia materia;
+	    
+	    @Autowired
+	    private TestRestTemplate restTemplate;
+	    
+	    private String getRootUrl() {
+	        return "http://localhost:" + port;
+	    }
+	    
+	    @Test
+	    public void testGetAllEstudo() {
+	    	int user = 1;
+	        HttpHeaders headers = new HttpHeaders();
+	        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/usuario/"+ user + "/estudo/all",
+	                HttpMethod.GET, entity, String.class);
+	        assertNotNull(response.getBody());
+	    }
+	    
+	    
+	    @Test
+	    public void testCriarEstudo() throws ParseException {
+	    	int user = 1;
+	        estudo.setData("Teste");
+	        estudo.setUsuario(usuario);
+	        estudo.setMateria(materia);
+	        ResponseEntity<Estudo> postResponse = restTemplate.postForEntity(getRootUrl() + "/usuario/"+ user + "/estudo", estudo, Estudo.class );
+	        assertNotNull(postResponse);
+	        assertNotNull(postResponse.getBody());
+	    }
 	    
 }
--- backend/src/test/java/com/academicPlanner/academicPlanner/testeMateria.java
+++ backend/src/test/java/com/academicPlanner/academicPlanner/testeMateria.java
@@ -1,12 +1,63 @@
 package com.academicPlanner.academicPlanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.expression.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.academicPlanner.academicPlanner.Model.Materia;
+import com.academicPlanner.academicPlanner.Model.Usuario;
 
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AcademicPlannerApplication.class , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class testeMateria {
+	  @LocalServerPort
+	    private int port;
 
+	    @Autowired
+	    private Materia materia;
+	    
+	    private Usuario usuario;
+	    
+	    @Autowired
+	    private TestRestTemplate restTemplate;
+	    
+	    private String getRootUrl() {
+	        return "http://localhost:" + port;
+	    }
+	    
+	    @Test
+	    public void testGetAllMaterias() {
+	    	int user = 1;
+	        HttpHeaders headers = new HttpHeaders();
+	        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/usuario/"+user+"/materia/all",
+	                HttpMethod.GET, entity, String.class);
+	        assertNotNull(response.getBody());
+	    }
+	    
+	    
+	    @Test
+	    public void testCriarMaterias() throws ParseException {
+	    	int user = 1;
+	        materia.setDescricao("Teste");
+	        materia.setNome("NomeMateria");
+	        materia.setTipoMedia(1);
+	        materia.setUsuario(usuario);
+	        ResponseEntity<Materia> postResponse = restTemplate.postForEntity(getRootUrl() + "/usuario/"+user+"/materia", materia, Materia.class );
+	        assertNotNull(postResponse);
+	        assertNotNull(postResponse.getBody());
+	    }
+	    
 }
